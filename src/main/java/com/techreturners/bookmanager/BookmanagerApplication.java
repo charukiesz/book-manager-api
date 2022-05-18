cf7053af0a96427ce54d108202c7e492990c49f4
charukiesz <charulatapatel2002@yahoo.co.uk>
2022-05-18 21:12:59 +0100

    feat: add springdoc dependency in pom.xml, swaggerConfiguration method to Main

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -1,7 +1,9 @@
 package com.techreturners.bookmanager;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookmanagerApplication {
@@ -9,5 +11,22 @@ public class BookmanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagerApplication.class, args);
 	}
+
+	/* You are using the builder pattern here to construct the GroupedOpenApi instance.
+		It is a way of choosing how to build (construct) the GroupedOpenApi object by specifying
+		some construction steps.
+		The Special Builder object helps with the process of constructing.
+	 */
+	@Bean
+	public GroupedOpenApi swaggerConfiguration(){
+		return GroupedOpenApi.builder()
+				.group("book-manager-api")
+				.pathsToMatch("/api/v1/book/**")
+				.build();
+	}
+
+
+
+
 
 }
